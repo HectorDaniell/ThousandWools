11149cd168b474a0a5b74f3ed024188f57c0893e
Hector Daniel Oviedo <91428248+HectorDaniell@users.noreply.github.com>
2023-02-26 19:56:38 -0500

    Seccion galeria implementada

---
+++ js/imgFull.js
@@ -0,0 +1,19 @@
+const fulImgBox= document.getElementById("ful-img-box");
+const fulImg= document.getElementById("ful-img");
+const btnClose=document.getElementById("btncls");
+
+
+
+btnClose.addEventListener("click",closeImg);
+
+
+
+
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
+
+function openImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference;
+}
